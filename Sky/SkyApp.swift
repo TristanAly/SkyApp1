1fcd7fac681bfdf27f1b87bfd93d940f34fb477f
AlyTristan <tristan.aly@gmail.com>
2022-05-30 15:53:59 +0200

    Initial Commit

---
+++ Sky/SkyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkyApp.swift
+//  Sky
+//
+//  Created by Tristan Aly on 30/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SkyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
